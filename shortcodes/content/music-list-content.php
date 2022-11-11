f29fcdd33f7552b9c21856f32321ef9a1e3ffffd
XTR\daniardy.pradana <daniardy.pradana@xtremax.com>
2022-11-12 06:14:52 +0700

    add the shortcut style

--- shortcodes/content/music-list-content.php
+++ shortcodes/content/music-list-content.php
@@ -3,13 +3,22 @@ $args = array(
 	'post_type'      => 'my_music',
 	'posts_per_page' => 10,
 );
-$loop = new WP_Query($args);
-while ( $loop->have_posts() ) {
-	$loop->the_post();
-	?>
-	<div class="entry-content">
-		<?php the_title(); ?>
-		<div><img src="<?php echo get_post_meta(get_the_ID() , 'dnmc_music_image', true)['url'] ?>" /></div>
-	</div>
-	<?php
-}
+$loop = new WP_Query($args); ?>
+<div class="container">
+    <div class="row d-flex flex-row">
+        <?php
+        while ( $loop->have_posts() ) {
+            $loop->the_post();
+            ?>
+            <div class="col-lg-5 mx-3 musicCard">
+            <a href="<?php echo get_post_permalink(); ?>">
+                <h4>
+                    <?php the_title(); ?>
+                </h4>
+                <img src="<?php echo get_post_meta(get_the_ID() , 'dnmc_music_image', true)['url'] ?>" />
+            </a>
+            </div>
+            <?php
+        } ?>
+    </div>
+</div>
--- shortcodes/music-list.php
+++ shortcodes/music-list.php
@@ -8,9 +8,30 @@ class DNMC_Music_List{
     }
 
     public function render_content($atts = [], $content = null){
-        include DNMC_PATH . '/shortcodes/content/music-list-content.php';
+        if(!$this->is_edit_page()){
+            $this->enqueue_scripts();
+            include DNMC_PATH . '/shortcodes/content/music-list-content.php';
+        }
 
         return ob_get_clean();
+    }
+
+    function enqueue_scripts() {
+        wp_enqueue_style( 'bootstrap-style', DNMC_URL . '/assets/css/bootstrap.min.css' );
+        wp_enqueue_script( 'bootstrap-script', DNMC_URL . '/assets/js/bootstrap.bundle.min.js');
+        wp_enqueue_style( 'main-style', DNMC_URL . '/assets/css/main.css' );
+    }
+
+    function is_edit_page($new_edit = null){
+        global $pagenow;
+        if (!is_admin()) return false;
+    
+        if($new_edit == "edit")
+            return in_array( $pagenow, array( 'post.php',  ) );
+        elseif($new_edit == "new")
+            return in_array( $pagenow, array( 'post-new.php' ) );
+        else
+            return in_array( $pagenow, array( 'post.php', 'post-new.php' ) );
     }
 }
 
